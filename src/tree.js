5e2cd84a20be8f8f71886b0cbf38b3071811cf5d
DenQ <denquick@gmail.com>
2017-09-02 15:39:36 +0300

    Tree exists

---
+++ src/tree.js
@@ -0,0 +1,7 @@
+module.exports = class Tree {
+
+  constructor() {
+    
+  }
+
+}
--- test/tree.js
+++ test/tree.js
@@ -3,4 +3,17 @@
   , expect = chai.expect
   , should = chai.should();
 
-// var Node = require('../node');
+let Tree = require('../src/tree');
+let tree = new Tree();
+
+describe('Tree', function() {
+  beforeEach(function() {
+    tree = new Tree();
+  });
+
+  it('It exists', function() {
+    expect(tree instanceof Tree).to.equal(true);
+
+  });
+
+});
